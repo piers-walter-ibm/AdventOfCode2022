bf651bb5d29723b357817ca88fac976386f725df
Piers Walter <Piers.Walter@ibm.com>
2022-12-01 15:13:15 +0000

    Update day1_golf.py

--- 01/day1_golf.py
+++ 01/day1_golf.py
@@ -1,3 +1,3 @@
 n="\n"
-c=sorted(list(map(lambda x: sum(list(map(int,x.split(n)))),open("input.txt").read().split(n*2))))
+c=sorted(list(map(lambda x:sum(list(map(int,x.split(n)))),open("input.txt").read().split(n*2))))
 print(c[-1],sum(c[-3:]))
